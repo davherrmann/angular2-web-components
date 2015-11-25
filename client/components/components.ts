8c2437dccdff3c34162bc298db38e1c83ab7bb13
David Herrmann <davherrmann@googlemail.com>
2015-11-25 12:30:06 +0100

    revert deletion

---
+++ client/components/components.ts
@@ -0,0 +1,5 @@
+import {ActionsCmp} from './actions/actions';
+import {FormCmp} from './form/form';
+import {FrameCmp} from './frame/frame';
+
+export const ALL_COMPONENTS: any[] = [FormCmp, FrameCmp, ActionsCmp];
